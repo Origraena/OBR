02da767088f124302daa43327ea6c5e85c8d797d
Swan Rocher <swan.rocher@gmail.com>
2012-03-15 07:32:51 +0100

    demo scripts

--- dev/src/Demo20120314.java
+++ dev/src/Demo20120314.java
@@ -11,20 +11,9 @@ public class Demo20120314 {
 		try {
 			System.out.println("Reading file : "+args[0]);
 			GraphRuleDependencies grd = new GraphRuleDependencies(args[0]);
-//			grd.add(new AtomicRule("p<x,y>;q<y>-->r<x,y,z>"));
-//			grd.add(new AtomicRule("r<x,y,z>;r<x,y,x>-->p<x,x>"));
-//			grd.add(new AtomicRule("r<'a','b',x>-->r<x,x,y>"));
-//			grd.add(new AtomicRule("p<x,y>;p<'a',y>-->p<'a',z>"));
 
 			System.out.println(grd);
 		
-/*			for (int i = 0 ; i < grd.getNbComponents() ; i++) {
-				System.out.println("i = "+i);
-				System.out.println(grd.getComponent(i));
-			}
-*/
-
-//			DirectedSimpleGraph<ArrayList<Vertex<AtomicRule> >,Boolean> grd_scc = grd.getStronglyConnectedComponentsGraph();
 			System.out.println(grd.stronglyConnectedComponentsToString());
 
 		}
